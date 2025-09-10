b8f9606d8dd97ed9014188fd1c18d3f623857595
bilalshafeek <bilal.shafeekpn@gmail.com>
2025-09-10 17:08:23 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "aws_region" {}
+variable "vpc_cidr" {}
+variable "public_subnet_cidr" {}
+variable "availability_zone" {}
+variable "instance_type" {}
+variable "ami_id" {}
+variable "key_name" {}
